144bb9b44f925a932413ea60c3ebf1f3f3d765bb
nngogol <nngogol09@gmail.com>
2017-10-29 00:35:35 +0300

    deleting old walkers
    walks[n].wannaDie() == 1 - for deleting

--- sketch.js
+++ sketch.js
@@ -4,19 +4,22 @@ let endPos;
 
 function setup() {
 	createCanvas(700,700);
+	walks.push( new Walker(40,60))
+	walks.push( new Walker(40,60))
 }
 
 function draw() {
 	background(151)
 
-	for (let n = walks.length-1; n < 0 ; n--) {
-		if(walks[n].wannaDie())
-			walks[n].slice(n,1)
-		else
-			walks[n].move();
+	for (let n = walks.length-1; n > 0 ; n--) {
+		if(walks[n].wannaDie() == 1)
+			walks.splice(n,1)
+		
 	}
-
+	
+	walks.map(x => x.move())
 	walks.map(x => x.render())
+		
 }
 
 function mousePressed(){
@@ -28,6 +31,7 @@ function mouseClicked(){
 }
 
 class Walker{
+
 	constructor(x = 200, y = 200, angle = random(0,360)){
 		this.initPos = createVector(x, y);
 		this.pos = createVector(0, 0)
@@ -44,37 +48,7 @@ class Walker{
 	}
 
 	wannaDie(){
-		return this.frame > 2000 ? true : false
-	}
-
-	renderTrail() {
-		
-		stroke(0,255,0)
-		fill(0,60,60)
-		let sizeEllipse = 14;
-		for(let i = this.history.length-1; i >= 0; i--){
-			
-				ellipse(this.history[i].x,
-					this.history[i].y,
-					sizeEllipse,
-					sizeEllipse);
-
-				sizeEllipse -= 0.3;
-		}
-	}
-
-	trailAdd(){
-		
-		// done
-		// добавить
-		this.history.push(this.pos.copy());
-		
-		// done
-		// удалить лишнее
-		if(this.history.length > this.trailLenght){
-			this.history.splice(0,1);
-		}
-		
+		return this.frame > 200 ? 1 : 0
 	}
 
 	move(){
@@ -87,18 +61,12 @@ class Walker{
 		this.pos.y += sin(this.frame*.3)
 	}
 
-	renderWalker(){
-		stroke(255)
-		fill(255,40,20)
-
-		ellipse(this.pos.x, this.pos.y, 16, 16);
-		
-	}
-
 	render(){
 		push()
+
 		translate(this.initPos.x, this.initPos.y)
 		rotate(radians(this.angle))
+
 		stroke(255)
 		noFill()
 
@@ -108,8 +76,36 @@ class Walker{
 		this.renderTrail()
 		this.renderWalker()
 
-
-
 		pop()
+	}
+
+	renderWalker(){
+		stroke(255)
+		fill(255,40,20)
+		// text(this.frame,40,40)
+		ellipse(this.pos.x, this.pos.y, 16, 16);
+	}
+
+	renderTrail() {
+		stroke(0,255,0)
+		fill(0,60,60)
+
+		let sizeEllipse = 14;
+		for(let i = this.history.length-1; i >= 0; i--){
+				ellipse(this.history[i].x,this.history[i].y, sizeEllipse, sizeEllipse);
+				sizeEllipse -= 0.3;
+		}
+	}
+
+	trailAdd(){
+		
+		// done
+		// добавить
+		this.history.push(this.pos.copy());
+		
+		// done
+		// удалить лишнее
+		if(this.history.length > this.trailLenght)
+			this.history.splice(0,1);
 	}
 }
